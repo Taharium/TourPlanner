cbffd1b2b60646e863000d03a4f0006f369b01ed
Taharium <if22b207@technikum-wien.at>
2024-02-21 13:43:20 +0100

    test

--- Tour_Planner/MainWindow.xaml.cs
+++ Tour_Planner/MainWindow.xaml.cs
@@ -1,14 +1,11 @@
 ﻿using System.Windows;
 
-namespace Tour_Planner
-{
+namespace Tour_Planner {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
         }
     }
