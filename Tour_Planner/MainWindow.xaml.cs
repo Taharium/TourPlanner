96886b39706e02ef7f32e762d0c2917db063f374
Taharium <if22b207@technikum-wien.at>
2024-02-28 10:55:08 +0100

    added Image and "Table"

--- Tour_Planner/MainWindow.xaml.cs
+++ Tour_Planner/MainWindow.xaml.cs
@@ -9,5 +9,8 @@ namespace Tour_Planner {
             InitializeComponent();
         }
 
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e) {
+
+        }
     }
 }
