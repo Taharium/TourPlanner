bab1b67f29346dbd35262336e42e521aa467f26b
Taharium <if22b207@technikum-wien.at>
2024-03-31 14:42:44 +0200

    Update TourLogs.cs

--- Models/TourLogs.cs
+++ Models/TourLogs.cs
@@ -1,14 +1,85 @@
-﻿using Tour_Planner.Enums;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Tour_Planner.Enums;
 
 namespace Models {
-    public class TourLogs {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        public DateTime DateTime { get; set; } = DateTime.Now;
-        public string TotalTime { get; set; } = "";
-        public string Distance { get; set; } = "";
-        public Rating Rating { get; set; } = Rating.Excellent;
-        public string Comment { get; set; } = "";
-        public Difficulty Difficulty { get; set; } = Difficulty.Easy;
+    public class TourLogs : INotifyPropertyChanged {
+
+        private Guid _id = Guid.NewGuid();
+        private DateTime _dateTime = DateTime.Now;
+        private string _totalTime = "";
+        private string _distance = "";
+        private Difficulty _difficulty = Difficulty.Easy;
+        private string _comment = "";
+        private Rating _rating = Rating.Excellent;
+
+        public Guid Id {
+            get => _id;
+            set {
+                if (_id != value) {
+                    _id = value;
+                    RaisePropertyChanged(nameof(Id));
+                }
+            }
+        }
+
+        public DateTime DateTime {
+            get => _dateTime;
+            set {
+                if (_dateTime != value) {
+                    _dateTime = value;
+                    RaisePropertyChanged(nameof(DateTime));
+                }
+            }
+        }
+        public string TotalTime {
+            get => _totalTime;
+            set {
+                if (_totalTime != value) {
+                    _totalTime = value;
+                    RaisePropertyChanged(nameof(TotalTime));
+                }
+            }
+        }
+        public string Distance {
+            get => _distance;
+            set {
+                if (_distance != value) {
+                    _distance = value;
+                    RaisePropertyChanged(nameof(Distance));
+                }
+            }
+        }
+
+        public Rating Rating {
+            get => _rating;
+            set {
+                if (_rating != value) {
+                    _rating = value;
+                    RaisePropertyChanged(nameof(Rating));
+                }
+            }
+        }
+
+        public string Comment {
+            get => _comment;
+            set {
+                if (_comment != value) {
+                    _comment = value;
+                    RaisePropertyChanged(nameof(Comment));
+                }
+            }
+        }
+
+        public Difficulty Difficulty {
+            get => _difficulty;
+            set {
+                if (_difficulty != value) {
+                    _difficulty = value;
+                    RaisePropertyChanged(nameof(Difficulty));
+                }
+            }
+        }
 
         public TourLogs() { }
 
@@ -20,6 +91,20 @@ namespace Models {
             Rating = tourLogs.Rating;
             Comment = tourLogs.Comment;
             Difficulty = tourLogs.Difficulty;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = "") {
+
+            ValidatePropertyName(propertyName);
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ValidatePropertyName(string propertyName) {
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null) {
+                throw new ArgumentException("Property not found", propertyName);
+            }
         }
     }
 }
