7002ef5944a984240e75e374929d616e5aab7158
PaulFel123 <if22b151@technikum-wien.at>
2024-04-22 14:45:37 +0200

    Added Project(DAL), added appsettings where connectionstringis)

---
+++ DataAccessLayer/TourPlannerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccessLayer;
+
+public class TourPlannerDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+    
+    public TourPlannerDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("TourPlannerDb"));
+    }
+}
