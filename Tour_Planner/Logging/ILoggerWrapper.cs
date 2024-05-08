3b3c28c912801a27183498c20d5273d918d9c4d8
Taharium <if22b207@technikum-wien.at>
2024-05-08 11:38:20 +0200

    added Logger class

---
+++ Tour_Planner/Logging/ILoggerWrapper.cs
@@ -0,0 +1,8 @@
+﻿namespace Tour_Planner.Logging;
+
+public interface ILoggerWrapper {
+    void Debug(string message);
+    void Error(string message);
+    void Fatal(string message);
+    void Warn(string message);
+}
