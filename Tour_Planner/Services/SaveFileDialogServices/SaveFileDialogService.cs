c465a9eb3e96f0c8b6bbacb3c64790fb028d67a8
Taharium <if22b207@technikum-wien.at>
2024-05-22 11:43:34 +0200

    removed bug

--- Tour_Planner/Services/SaveFileDialogServices/SaveFileDialogService.cs
+++ Tour_Planner/Services/SaveFileDialogServices/SaveFileDialogService.cs
@@ -11,8 +11,8 @@ public class SaveFileDialogService : ISaveFileDialogService {
         _saveFileDialog.InitialDirectory = @"C:\";
         _saveFileDialog.Title = "Save text Files";
         _saveFileDialog.CheckPathExists = true;
-        _saveFileDialog.DefaultExt = "pdf";
-        _saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf|All Files (*.*)|*.*";
+        _saveFileDialog.DefaultExt = "json";
+        _saveFileDialog.Filter = "JSON Files (*.json)|*.json|All Files (*.*)|*.*";
         _saveFileDialog.FilterIndex = 2;
         _saveFileDialog.RestoreDirectory = true;
         _saveFileDialog.FileName = fileName;
